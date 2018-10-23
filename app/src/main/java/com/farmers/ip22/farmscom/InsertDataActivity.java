0e80290284ff44ee0b74c2eb77657a03de475676
kingsley555 <kingsleyoha@hotmail.com>
2018-10-23 15:30:43 +0200

    UI design

---
+++ app/src/main/java/com/farmers/ip22/farmscom/InsertDataActivity.java
@@ -0,0 +1,13 @@
+package com.farmers.ip22.farmscom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_data);
+    }
+}
